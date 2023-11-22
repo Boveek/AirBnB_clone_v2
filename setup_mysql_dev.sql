3b14e31f2a1c9ee56a801f69b66c77216738cc52
Ololade Victor <oluwabori2014@gmail.com>
2023-11-22 03:29:23 -0800

    AirBnB clone version 2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,11 @@
+-- Create hbnb_dev_db database if not in existence
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create User hbnb_dev if not in existence with hbnb_dev_pwd as password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges of the database to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Reload privileges to apply changes
 FLUSH PRIVILEGES;
 
